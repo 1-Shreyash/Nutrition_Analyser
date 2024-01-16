df5b9a81f398f3d7c63c012f4d05872e25b9aa3b
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-01-16 11:51:55 +0530

    Samrat Auth

--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -4,6 +4,17 @@ import Recipe from "./Recipe";
 const Repo = () => {
   return (
     <div className="grid grid-cols-4">
+      {/* {Food.map((food, index) => {
+            return (
+              <tr key={index}>
+                <td className="p-2">{food.name}</td>
+                <td className="p-2">{food.calories}</td>
+                <td className="p-2">{food.serving_size_g}</td>
+                <td className="p-2">{food.sugar_g}</td>
+                <td className="p-2">{food.fat_total_g}</td>
+              </tr>
+            );
+          })} */}
       <Recipe
         dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
         header="Lakes"
