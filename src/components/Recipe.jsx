a5e5c985f62b72db8c6ba63d07971f9c958f72b0
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-01-16 11:17:03 +0530

    done

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import "../style/Rc.scss"
 
-const Recipe = () => {
+
+const Recipe = ({dataImage, header, content}) => {
+  const [mouseX, setMouseX] = React.useState(0);
+  const [mouseY, setMouseY] = React.useState(0);
+  const [cardDimensions, setCardDimensions] = React.useState({width: 0, height: 0});
+  const cardRef = React.useRef();
+
+  React.useEffect(() => {
+    setCardDimensions({
+      width: cardRef.current.offsetWidth,
+      height: cardRef.current.offsetHeight
+    });
+  }, []);
+
+  const handleMouseMove = (e) => {
+    setMouseX(e.pageX - cardRef.current.offsetLeft - cardDimensions.width / 2);
+    setMouseY(e.pageY - cardRef.current.offsetTop - cardDimensions.height / 2);
+  }
+
+  const handleMouseLeave = () => {
+    setTimeout(() => {
+      setMouseX(0);
+      setMouseY(0);
+    }, 1000);
+  }
+
+  const cardStyle = {
+    transform: `rotateY(${mouseX / cardDimensions.width * 30}deg) rotateX(${mouseY / cardDimensions.height * -30}deg)`
+  };
+
+  const cardBgTransform = {
+    transform: `translateX(${mouseX / cardDimensions.width * -40}px) translateY(${mouseY / cardDimensions.height * -40}px)`
+  };
+
+  const cardBgImage = {
+    backgroundImage: `url(${dataImage})`
+  };
+
   return (
-    <div>
-        
+    <div className="card-wrap" 
+      onMouseMove={handleMouseMove} 
+      onMouseEnter={() => clearTimeout(handleMouseLeave)} 
+      onMouseLeave={handleMouseLeave} 
+      ref={cardRef}>
+      <div className="card" style={cardStyle}>
+        <div className="card-bg" style={{...cardBgTransform, ...cardBgImage}}></div>
+        <div className="card-info">
+          <h1>{header}</h1>
+          <p>{content}</p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Recipe
+export default Recipe;
--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import Recipe from "./Recipe";
 
 const Repo = () => {
-  return <div>Repo</div>;
+  return (
+    <div className="grid grid-cols-4">
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <Recipe
+        dataImage="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop="
+        header="Lakes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </div>
+  );
 };
 
 export default Repo;
