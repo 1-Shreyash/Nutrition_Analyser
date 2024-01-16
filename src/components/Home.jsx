670855c6252d08b3f3b8e9fb3646e77c5810cbf8
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-01-16 15:40:09 +0530

    done 123

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import himg from "../resources/HFimg.gif";
 import NutritionAnalysis from "../resources/NutritionAnalysis.jpeg";
 import MealPlanner from "../resources/MealPlanner.webp";
-
+import UI from "../resources/UI2.gif"
 
 const Home = () => {
   return (
@@ -13,7 +13,7 @@ const Home = () => {
           self-respect. Choose foods that love you back.
         </div>
         <div className="w-[50%] flex items-center justify-center">
-          <img className="w-[70%]" src={himg} alt="" />
+          <img className="w-[70%] m-12" src={himg} alt="" />
         </div>
       </div>
       <div className="flex justify-center items-center text-5xl my-16 lemon-regular pt-6">
@@ -21,51 +21,42 @@ const Home = () => {
       </div>
 
       <div className="w-full flex flex-row my-8">
-        <div className="flex flex-col justify-center items-center text-center w-[70%]">
+        <div className="flex flex-col justify-center items-center text-center w-[70%] p-24">
           <div className="flex justify-center items-center text-4xl text-green-800 lemon-regular pt-6">
             NUTRITION ANALYSER
           </div>
           <div className="text-2xl font-bold  pt-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Voluptatibus voluptates at porro harum architecto laboriosam
-            doloremque libero molestias, est, eveniet sit. Minima, totam.
-            Laborum explicabo fuga id vitae temporibus exercitationem odio fugit
-            quis corporis.
+            
+Introducing a Nutrition Analysis App: Effortlessly log meals, analyze macronutrients, and set personalized dietary goals for a comprehensive view of your nutritional intake. Elevate your well-being with a tool that goes beyond calorie counting, providing education and insights for a balanced and sustainable lifestyle.
           </div>
         </div>
-        <img src={NutritionAnalysis} className="w-[30%]" alt="" />
+        <img src={NutritionAnalysis} className="w-[30%] m-12" alt="" />
       </div>
 
       <div className="w-full flex flex-row my-8">
-        <div className="flex flex-col justify-center items-center text-center w-[70%]">
+        <img src={MealPlanner} className="w-[30%] h-80" alt="" />
+        <div className="flex flex-col justify-center items-center text-center w-[70%] p-24">
           <div className="flex justify-center items-center text-4xl text-green-800 lemon-regular pt-6">
             MEAL PLANNER
           </div>
           <div className="text-2xl font-bold  pt-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Voluptatibus voluptates at porro harum architecto laboriosam
-            doloremque libero molestias, est, eveniet sit. Minima, totam.
-            Laborum explicabo fuga id vitae temporibus exercitationem odio fugit
-            quis corporis.
+            
+Introducing a Nutrition Analysis App: Effortlessly log meals, analyze macronutrients, and set personalized dietary goals for a comprehensive view of your nutritional intake. With features like barcode scanning, allergen alerts, and progress tracking, this user-friendly app empowers individuals to make informed and healthy choices. Elevate your well-being with a tool that goes beyond calorie counting, providing education and insights for a balanced and sustainable lifestyle.
           </div>
         </div>
-        <img src={MealPlanner} className="w-[30%]" alt="" />
       </div>
 
       <div className="w-full flex flex-row my-8">
-        <div className="flex flex-col justify-center items-center text-center w-[70%]">
+        <div className="flex flex-col justify-center items-center text-center w-[70%] p-24">
           <div className="flex justify-center items-center text-4xl text-green-800 lemon-regular pt-6">
-            NUTRITION ANALYSER
+            INTERACTIVE UI
           </div>
           <div className="text-2xl font-bold  pt-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Voluptatibus voluptates at porro harum architecto laboriosam
-            doloremque libero molestias, est, eveniet sit. Minima, totam.
-            Laborum explicabo fuga id vitae temporibus exercitationem odio fugit
-            quis corporis.
+            
+Immerse yourself in a seamless online experience with our website's interactive UI. Navigate effortlessly through dynamic menus, engaging visuals, and personalized content tailored to your interests. Elevate user satisfaction with a responsive interface that transforms browsing into a captivating journey.
           </div>
         </div>
-        <img src={NutritionAnalysis} className="w-[30%]" alt="" />
+        <img src={UI} className="w-[30%] m-12" alt="" />
       </div>
     </div>
   );
--- src/components/MealPlanner.jsx
+++ src/components/MealPlanner.jsx
@@ -77,7 +77,7 @@ const MealPlanner = () => {
       const response = await axios.request(options);
       console.log(response.data);
       let updatedData = [response.data.hits];
-      // console.log(response.data.hits);
+      console.log(response.data.hits);
       setFood(updatedData);
       localStorage.setItem("foodEat", JSON.stringify(updatedData));
     } catch (error) {
@@ -93,100 +93,82 @@ const MealPlanner = () => {
   }, []);
 
   return (
-    <div className="w-full min-h-screen flex flex-col box-border items-center justify-center border-4 border-blue-900">
-      <div className="m-6 text-5xl font-bold">Meal Planner:</div>
-      <div className="flex flex-row p-4">
-        <label className="mb-2 text-lg">Cuisine:</label>
-        <select
-          className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-          name="cuisineType"
-          onChange={handleChange}
-        >
-          <option value="">-- Select --</option>
-          <option value="chinese">chinese</option>
-          {/* <option value="clothing">Clothing</option>
-            <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option> */}
-        </select>
+    <div className="w-full flex flex-col items-center mt-4">
+      <div className="m-6 text-5xl font-bold">Meal Planner</div>
+      <div className="w-[60%]">
+        <div className="p-2">
+          <label className="mb-2 text-lg">Cuisine:</label>
+          <br />
+          <select
+            className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
+            name="cuisineType"
+            onChange={handleChange}
+          >
+            <option value="">-- Select --</option>
+            <option value="chinese">Chinese</option>
+            <option value="south american">South American</option>
+            <option value="mediterranean">Mediterranean</option>
+            <option value="american">American</option>
+            <option value="korean">Korean</option>
+            <option value="mexican">Mexican</option>
+            <option value="indian">Indian</option>
+          </select>
+        </div>
+        <div className="p-2">
+          <label className="mb-2 text-lg">Meal Type:</label>
+          <br />
+          <select
+            className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
+            name="mealType"
+            onChange={handleChange}
+          >
+            <option value="">-- Select --</option>
+            <option value="lunch/dinner">Lunch/Dinner</option>
+            <option value="breakfast">Breakfast</option>
+            <option value="teatime">Teatime</option>
+          </select>
+        </div>
+        <div className="p-2">
+          <label className="mb-2 text-lg">Health Label: </label>
+          <br />
+          <select
+            className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
+            name="healthLabels"
+            onChange={handleChange}
+          >
+            <option value="">-- Select --</option>
+            <option value="Sugar-Conscious">Sugar-Conscious</option>
+            <option value="Vegan">Vegan</option>
+            <option value="Alcohol-Free">Alcohol-Free</option>
+            <option value="Keto-Friendly">Keto-Friendly</option>
+            <option value="Vegetarian">Vegetarian</option>
+            <option value="Tree-Nut-Free">Tree-Nut-Free</option>
+          </select>
+        </div>
+        <div className="p-2">
+          <label className="mb-2 text-lg">Dish Type: </label>
+          <br />
+          <select
+            className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
+            name="dishType"
+            onChange={handleChange}
+          >
+            <option value="">-- Select --</option>
+            <option value="starter">Starter</option>
+            <option value="bread">Bread</option>
+            <option value="desserts">Desserts</option>
+            <option value="cereals">Cereals</option>
+            <option value="biscuits and cookies">Biscuits and Cookies</option>
+            <option value="salad">Salad</option>
+          </select>
+        </div>
       </div>
-      <div className="flex flex-row p-4">
-        <label className="mb-2 text-lg">Meal Type:</label>
-        <select
-          className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-          name="mealType"
-          onChange={handleChange}
-        >
-          <option value="">-- Select --</option>
-          <option value="lunch/dinner">lunch/dinner</option>
-          {/* <option value="clothing">Clothing</option>
-            <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option> */}
-        </select>
-      </div>
-      <div className="flex flex-row p-4">
-        <label className="mb-2 text-lg">Health Label: </label>
-        <select
-          className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-          name="healthLabels"
-          onChange={handleChange}
-        >
-          <option value="">-- Select --</option>
-          <option value="Sugar-Conscious">Sugar-Conscious</option>
-          <option value="Keto-Friendly">Keto-Friendly</option>
-          <option value="Vegetarian">Vegetarian</option>
-          <option value="Tree-Nut-Free">Tree-Nut-Free</option>
-        </select>
-      </div>
-      <div className="flex flex-row p-4">
-        <label className="mb-2 text-lg">Dish Type: </label>
-        <select
-          className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-          name="dishType"
-          onChange={handleChange}
-        >
-          <option value="">-- Select --</option>
-          <option value="sstarterhoes">starter</option>
-          {/* <option value="clothing">Clothing</option>
-            <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option> */}
-        </select>
-      </div>
-
-      {/* <div className="flex flex-row p-4">
-        <label htmlFor="cars">Cuisine: </label>
-        <input type="text" className="border-2 mx-2" ref={FoodName} />
-      </div> */}
       <button
-        className="bg-black p-2 rounded-lg text-white"
+        className="mt-4 bg-slate-800 duration-300 hover:bg-slate-600 p-2 rounded-lg text-xl text-white w-40"
         onClick={giveRecipe}
       >
         Submit
       </button>
-      {/*
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="p-2">Food Name : </th>
-            <th className="p-2">Calories : </th>
-            <th className="p-2">Serving Size : </th>
-            <th className="p-2">Sugar: </th>
-            <th className="p-2">Fat : </th>
-          </tr>
-        </thead>
-        <tbody className="p-4 mx-2 border-2 border-red-900">
-          {Food.map((food, index) => {
-            return (
-              <tr key={index}>
-                <td className="p-2">{food.name}</td>
-                <td className="p-2">{food.calories}</td>
-                <td className="p-2">{food.serving_size_g}</td>
-                <td className="p-2">{food.sugar_g}</td>
-                <td className="p-2">{food.fat_total_g}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table> */}
     </div>
   );
 };
