b95e31b3c0bc5a316ffc50036262c454539a375d
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-01-16 10:19:51 +0530

    Co-authored-by: Sahil Hedau <sahilhedau49@users.noreply.github.com>

--- src/components/MealPlanner.jsx
+++ src/components/MealPlanner.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import axios from "axios";
 
 const MealPlanner = () => {
-  // const [Food, setFood] = useState([]);
+  const [Food, setFood] = useState([]);
 
   const [formData, setFormData] = useState({
     cuisineType: "",
@@ -22,6 +22,7 @@ const MealPlanner = () => {
   const FoodName = useRef(null);
   const FoodQuantity = useRef(null);
   
+  //Food
   const FetchData = async () => {
     var q = FoodQuantity.current.value + "g " + FoodName.current.value;
     console.log(q);
@@ -56,10 +57,10 @@ const MealPlanner = () => {
         "field[0]": "uri",
         beta: "true",
         random: "true",
-        "cuisineType[0]": "chinese",
-        "mealType[0]": "lunch/dinner",
-        "health[0]": "Sugar-Conscious",
-        "dishType[0]": "starter",
+        "cuisineType[0]": formData.cuisineType,
+        "mealType[0]": formData.mealType,
+        "health[0]": formData.health,
+        "dishType[0]": formData.dishType
       },
       headers: {
         "Accept-Language": "en",
@@ -78,74 +79,75 @@ const MealPlanner = () => {
 
   return (
     <div className="w-full min-h-screen flex flex-col box-border items-center justify-center border-4 border-blue-900">
-      <div className="m-6 text-5xl font-bold">Meal Planner :</div>
+      <div className="m-6 text-5xl font-bold">Meal Planner:</div>
       <div className="flex flex-row p-4">
           <label className="mb-2 text-lg">Cuisine:</label>
           <select
             className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-            name="category"
+            name="cuisineType"
             onChange={handleChange}
           >
             <option value="">-- Select --</option>
-            <option value="shoes">Shoes</option>
-            <option value="clothing">Clothing</option>
+            <option value="chinese">chinese</option>
+            {/* <option value="clothing">Clothing</option>
             <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option>
+            <option value="fanstore">Fanstore</option> */}
           </select>
         </div>
       <div className="flex flex-row p-4">
-          <label className="mb-2 text-lg">Meal Type :</label>
+          <label className="mb-2 text-lg">Meal Type:</label>
           <select
             className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-            name="category"
+            name="mealType"
             onChange={handleChange}
           >
             <option value="">-- Select --</option>
-            <option value="shoes">Shoes</option>
-            <option value="clothing">Clothing</option>
+            <option value="lunch/dinner">lunch/dinner</option>
+            {/* <option value="clothing">Clothing</option>
             <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option>
+            <option value="fanstore">Fanstore</option> */}
           </select>
         </div>
       <div className="flex flex-row p-4">
-          <label className="mb-2 text-lg">Health : </label>
+          <label className="mb-2 text-lg">Health Label: </label>
           <select
             className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-            name="category"
+            name="healthLabels"
             onChange={handleChange}
           >
             <option value="">-- Select --</option>
-            <option value="shoes">Shoes</option>
-            <option value="clothing">Clothing</option>
-            <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option>
+            <option value="Sugar-Conscious">Sugar-Conscious</option>
+            <option value="Keto-Friendly">Keto-Friendly</option>
+            <option value="Vegetarian">Vegetarian</option>
+            <option value="Tree-Nut-Free">Tree-Nut-Free</option>
           </select>
         </div>
       <div className="flex flex-row p-4">
-          <label className="mb-2 text-lg">Dish Type : </label>
+          <label className="mb-2 text-lg">Dish Type: </label>
           <select
             className="mb-3 p-2 w-full border-2 hover:border-slate-400 rounded"
-            name="category"
+            name="dishType"
             onChange={handleChange}
           >
             <option value="">-- Select --</option>
-            <option value="shoes">Shoes</option>
-            <option value="clothing">Clothing</option>
+            <option value="sstarterhoes">starter</option>
+            {/* <option value="clothing">Clothing</option>
             <option value="accessories">Accessories</option>
-            <option value="fanstore">Fanstore</option>
+            <option value="fanstore">Fanstore</option> */}
           </select>
         </div>
 
-      <div className="flex flex-row p-4">
+       {/* <div className="flex flex-row p-4">
         <label htmlFor="cars">Cuisine: </label>
         <input type="text" className="border-2 mx-2" ref={FoodName} />
-      </div>
+      </div> */}
       <button
         className="bg-black p-2 rounded-lg text-white"
         onClick={giveRecipe}
       >
         Submit
       </button>
+      {/*
       <table className="table">
         <thead>
           <tr>
@@ -169,7 +171,7 @@ const MealPlanner = () => {
             );
           })}
         </tbody>
-      </table>
+      </table> */}
     </div>
   );
 };
